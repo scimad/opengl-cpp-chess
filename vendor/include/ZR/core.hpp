db64f8fde95aababb2e8e87533cbf3b9172007d0
scimad <scimad2.71@gmail.com>
2022-12-16 06:43:19 +0545

    Improve logging function

--- vendor/include/ZR/core.hpp
+++ vendor/include/ZR/core.hpp
@@ -8,6 +8,7 @@ namespace zr{
     enum VERBOSITY_LEVEL{
         DEBUG = 0, INFO = 1, WARNING = 2, ERROR = 3
     };
+    extern std::string level_name[4];
     extern VERBOSITY_LEVEL log_level;
     void log(std::string, zr::VERBOSITY_LEVEL error_level=VERBOSITY_LEVEL::INFO);
 }
--- vendor/src/ZR/core.cpp
+++ vendor/src/ZR/core.cpp
@@ -2,11 +2,14 @@
 
 namespace zr{
 	VERBOSITY_LEVEL log_level = VERBOSITY_LEVEL::ERROR;
+	std::string level_name[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	// , "INFO", "WARNING", "ERROR"};
+
 	void log(std::string message, VERBOSITY_LEVEL level){
 		/* TODO:
 			* implement color for different verbosity level.
 		*/
 		if (level >= log_level)
-			std::cout << "\033[0;35m" << "[ZR] " "\033[1;32m" << message << "\033[0m" << std::endl;
+			std::cout << "\033[0;35m" << "[ZR] " "\033[1;32m" << zr::level_name[level] <<": " << message << "\033[0m" << std::endl;
 	}
 }
